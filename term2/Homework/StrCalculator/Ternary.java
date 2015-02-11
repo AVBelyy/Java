8a7b9b7af1c2c1b1c67270a7f6398ff21f72dffe
Anton Belyy <anton.belyy@gmail.com>
2015-02-11 21:43:03 +0300

    new semester

--- term2/Homework/StrCalculator/Ternary.java
+++ term2/Homework/StrCalculator/Ternary.java
@@ -1,6 +1,5 @@
 public class Ternary implements Expression3 {
     private final Expression3 exprA, exprB, exprC;
-    private static final double E = 1e-16;
 
     public Ternary(Expression3 exprA, Expression3 exprB, Expression3 exprC) {
         this.exprA = exprA;
@@ -8,7 +7,7 @@ public class Ternary implements Expression3 {
         this.exprC = exprC;
     }
 
-    public double evaluate(double x, double y, double z) {
-        return (Math.abs(exprA.evaluate(x, y, z)) > E) ? exprB.evaluate(x, y, z) : exprC.evaluate(x, y, z);
+    public int evaluate(int x, int y, int z) {
+        return (exprA.evaluate(x, y, z) != 0) ? exprB.evaluate(x, y, z) : exprC.evaluate(x, y, z);
     }
 }
---
+++ term4/Homework/RecursiveWalk/src/ru/ifmo/ctddev/belyy/walk/Main.java
@@ -0,0 +1,8 @@
+package ru.ifmo.ctddev.belyy.walk;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
